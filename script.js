de9c5249f605099c8bc4e3f1978a3edb01bbfa70
AymenXS <aymenghaloua@gmail.com>
2023-03-15 23:11:26 +0100

    Updating

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particleArray = [];
@@ -8,10 +8,23 @@ let particleArray = [];
 const mouse = {
   x: null,
   y: null,
-  radius: 150
-}
+  radius: 150,
+};
 
-window.addEventListener('mousemove', function(event){
+window.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-})
+});
+
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 0, 30);
+ctx.strokeStyle = "white";
+ctx.strokeRect(0, 0, 100, 100);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x,y){
+
+  }
+}
